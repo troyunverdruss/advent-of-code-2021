41ce5e10aacb5709951054d9c0cc97b1dfec16ae
Troy Unverdruss <troy@mini.local>
2021-11-29 19:51:39 -0800

    basic file reading and integer parsing

--- lib/day_01.ex
+++ lib/day_01.ex
@@ -1,5 +1,8 @@
 defmodule AOC.Day01 do
   def part1() do
-    IO.puts "hi"
+    string_data = File.read!("input/day01.txt") |> String.split("\n", trim: true)
+    data_numbers = Enum.map(string_data, fn x -> String.to_integer(x) end)
+    sum = Enum.at(data_numbers, 0) + Enum.at(data_numbers, 1)
+    IO.puts sum
   end
 end
