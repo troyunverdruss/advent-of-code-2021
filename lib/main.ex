7429fd48e57d7e5c4e26bdc5083043194b478d1a
troy <troy@unverdruss.net>
2021-12-01 09:29:01 -0800

    day 1

--- lib/main.ex
+++ lib/main.ex
@@ -1,14 +1,12 @@
 defmodule AOC.Main do
   use Application
-  #  use AOC
 
   def start(_type, _args) do
     IO.puts "starting"
 
-    Task.start(
-      fn ->
-        AOC.Day01.part1()
-      end
-    )
+    AOC.Day01.run()
+
+    children = []
+    Supervisor.start_link(children, strategy: :one_for_one)
   end
 end
