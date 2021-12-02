c8fb187ff80084729bed73d913b172344a943bdf
troy <troy@unverdruss.net>
2021-12-02 09:44:28 -0800

    refactor slightly

--- lib/day_02.ex
+++ lib/day_02.ex
@@ -2,16 +2,16 @@ defmodule AOC.Day02 do
   def run() do
     string_data = AOC.Utils.read_input_to_strings(2)
 
-    steps = Enum.map(
-      string_data,
-      fn line ->
-        case line do
-          "forward " <> forward -> {String.to_integer(forward), 0}
-          "down " <> down -> {0, String.to_integer(down)}
-          "up " <> up -> {0, -String.to_integer(up)}
-        end
-      end
-    )
+    steps = string_data
+            |> Enum.map(
+                 fn line ->
+                   case line do
+                     "forward " <> forward -> {String.to_integer(forward), 0}
+                     "down " <> down -> {0, String.to_integer(down)}
+                     "up " <> up -> {0, -String.to_integer(up)}
+                   end
+                 end
+               )
 
     distance = steps
                |> Enum.map(fn x -> elem(x, 0) end)
@@ -25,21 +25,19 @@ defmodule AOC.Day02 do
     part2_values = steps
                    |> Enum.reduce(
                         %{aim: 0, distance: 0, depth: 0},
-                        fn (x, acc) ->
-                          {distance, aim} = x
-                          if distance > 0 do
-                            if acc[:aim] > 0 do
-                              %{acc | distance: acc[:distance] + distance, depth: acc[:depth] + (distance * acc[:aim])}
-                            else
-                              %{acc | distance: acc[:distance] + distance}
-                            end
-                          else
-                            %{acc | aim: acc[:aim] + aim}
-                          end
-                        end
+                        fn (x, acc) -> part2_reducer(x, acc) end
                       )
 
 
     IO.puts("Part 2: #{part2_values[:depth] * part2_values[:distance]}")
+  end
+
+  defp part2_reducer(x, acc) do
+    {distance, aim} = x
+    if distance > 0 do
+      %{acc | distance: acc[:distance] + distance, depth: acc[:depth] + (distance * acc[:aim])}
+    else
+      %{acc | aim: acc[:aim] + aim}
+    end
   end
 end
