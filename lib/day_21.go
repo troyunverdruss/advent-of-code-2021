09e99acbb022aea9e6e07271dd65ac5b55a33a9c
Troy Unverdruss <tunverdruss@twitter.com>
2021-12-23 12:02:33 -0500

    day 21 part 2 in go

--- lib/day_21.go
+++ lib/day_21.go
@@ -16,8 +16,22 @@ func main() {
 	fmt.Printf("player 1 start: %d\n", player2Start)
 
 	part1 := playGame(player1Start, player2Start)
-
 	fmt.Println("Part 1: ", part1)
+
+	part2 := playGameDirac(player1Start, player2Start)
+	fmt.Println("Part 2: ", part2)
+	// too high 279504419265267
+}
+
+func playGameDirac(player1Start int, player2Start int) int64 {
+	player1 := Player{id: 1, start: player1Start, position: player1Start, score: 0}
+	player2 := Player{id: 2, start: player2Start, position: player2Start, score: 0}
+	result := takeTurn(make(map[string]Result), 0, []Player{player1, player2})
+	if result.p1Wins > result.p2Wins {
+		return result.p1Wins
+	} else {
+		return result.p2Wins
+	}
 }
 
 func playGame(player1Start int, player2Start int) int {
@@ -54,8 +68,76 @@ func playerTurn(player *Player, die *Die) {
 	//fmt.Printf("Player turn %d, rolled %d, position %d, score: %d, die rolls %d\n", player.id, rolled, player.position, player.score, die.rolls)
 }
 
+func playerTurnDirac(player *Player, roll int) {
+	player.position += roll
+	player.position = modStartAt1(player.position, 10)
+	player.score += player.position
+}
+
+type Result struct {
+	p1Wins int64
+	p2Wins int64
+}
+
+func takeTurn(memo map[string]Result, playerTurn int, players []Player) Result {
+	key := makeKey(
+		playerTurn,
+		players[0].score,
+		players[0].position,
+		players[1].score,
+		players[1].position)
+	val, ok := memo[key]
+	if ok {
+		return val
+	}
+
+	result := Result{p1Wins: 0, p2Wins: 0}
+
+	for _, roll := range diracRollOptions() {
+		p1 := players[0]
+		p2 := players[1]
+
+		if playerTurn == 0 {
+			playerTurnDirac(&p1, roll)
+			if playerHasWonDirac(&p1) {
+				result.p1Wins += 1
+			} else {
+				tempResult := takeTurn(memo, (playerTurn+1)%2, []Player{p1, p2})
+				result.p1Wins += tempResult.p1Wins
+				result.p2Wins += tempResult.p2Wins
+			}
+		} else {
+			playerTurnDirac(&p2, roll)
+			if playerHasWonDirac(&p2) {
+				result.p2Wins += 1
+			} else {
+				tempResult := takeTurn(memo, (playerTurn+1)%2, []Player{p1, p2})
+				result.p1Wins += tempResult.p1Wins
+				result.p2Wins += tempResult.p2Wins
+			}
+		}
+	}
+	memo[key] = result
+	return result
+}
+
+func makeKey(playerTurn int, p1Score int, p1Pos int, p2Score int, p2Pos int) string {
+	return fmt.Sprintf(
+		"%d %d %d %d %d",
+		playerTurn,
+		p1Score,
+		p1Pos,
+		p2Score,
+		p2Pos,
+	)
+}
+
 func playerHasWon(player *Player) bool {
 	return player.score >= 1000
+}
+
+func playerHasWonDirac(player *Player) bool {
+	return player.score >= 21
 }
 
 type Player struct {
@@ -68,6 +150,14 @@ type Player struct {
 type Die struct {
 	value int
 	rolls int
+}
+
+func diracRollOptions() []int {
+	return []int{3, 4, 5, 4, 5, 6, 5, 6, 7, 4, 5, 6, 5, 6, 7, 6, 7, 8, 5, 6, 7, 6, 7, 8, 7, 8, 9}
+}
+
+type DiracDie struct {
+	rollOptions []int
 }
 
 func (die *Die) roll() int {
--- lib/day_21_test.go
+++ lib/day_21_test.go
@@ -12,12 +12,56 @@ func Test_playGame(t *testing.T) {
 		args args
 		want int
 	}{
-		{"one", args{4,8}, 739785},
+		{"one", args{4, 8}, 739785},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := playGame(tt.args.player1Start, tt.args.player2Start); got != tt.want {
 				t.Errorf("playGame() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_modStartAt1(t *testing.T) {
+	type args struct {
+		n int
+		m int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"1", args{1, 10}, 1},
+		{"10", args{10, 10}, 10},
+		{"11 => 1", args{11, 10}, 1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := modStartAt1(tt.args.n, tt.args.m); got != tt.want {
+				t.Errorf("modStartAt1() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_playGameDirac(t *testing.T) {
+	type args struct {
+		player1Start int
+		player2Start int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int64
+	}{
+		{"part 2 example", args{player1Start: 4, player2Start: 8}, 444356092776315},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := playGameDirac(tt.args.player1Start, tt.args.player2Start); got != tt.want {
+				t.Errorf("playGameDirac() = %v, want %v", got, tt.want)
 			}
 		})
 	}
