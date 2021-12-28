badc19cd3cda7773001943f1397d6ec83fb56fd0
Troy Unverdruss <troyunverdruss@users.noreply.github.com>
2021-12-28 18:49:36 -0500

    parser tool playing

--- lib/parseday24.py
+++ lib/parseday24.py
@@ -6,14 +6,20 @@ groups =stringdata.split("\n\n")
 
 
 groups =map(lambda x: x.split("\n"), groups)
-groups=filter(lambda x: len(x) !=0, groups)
+#groups =map(lambda x: x.split("\n"), groups)
+#print(list(groups)[0])
+#print()
+#groups=filter(lambda x: len(x) !=0, groups)
 
-for g in filter(lambda x: len(x) !=0, groups):
-    print(g)
-    print()
+#for g in filter(lambda x: len(x) !=0, groups):
+#    print(g)
+#    print()
 
-groups2=map(lambda x: filter(lambda y: len(y)!=0), groups)
+groups2=[
+     x for x in list(filter(lambda y: len(y) !=0, groups))
+    ]
+
 #print(list(groups))
-
-z=zip(*groups2)
-print(list(z))
+print(groups2)
+#z=zip(*groups)
+#print(list(z))
