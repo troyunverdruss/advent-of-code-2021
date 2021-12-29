c07ff8412a86440b65493a129b81b685bdbd5234
Troy Unverdruss <tunverdruss@twitter.com>
2021-12-28 21:35:35 -0500

    fix the input formatter and add formatted text

--- lib/parseday24.py
+++ lib/parseday24.py
@@ -1,25 +1,31 @@
 with open("../input/day24.txt") as f:
-    stringdata = f.read()
-    
-stringdata=stringdata.replace("inp", "\ninp")
-groups =stringdata.split("\n\n")
+    string_data = f.read()
 
+string_data = string_data.replace("inp", "\ninp")
+groups = string_data.split("\n\n")
 
-groups =map(lambda x: x.split("\n"), groups)
-#groups =map(lambda x: x.split("\n"), groups)
-#print(list(groups)[0])
-#print()
-#groups=filter(lambda x: len(x) !=0, groups)
+groups = map(lambda x: x.split("\n"), groups)
+# groups =map(lambda x: x.split("\n"), groups)
+# print(list(groups)[0])
+# print()
+# groups=filter(lambda x: len(x) !=0, groups)
 
-#for g in filter(lambda x: len(x) !=0, groups):
+# for g in filter(lambda x: len(x) !=0, groups):
 #    print(g)
 #    print()
 
-groups2=[
-     x for x in list(filter(lambda y: len(y) !=0, groups))
-    ]
+list_groups = list(groups)
+groups2 = [
+    list(filter(lambda y: len(y) != 0, x)) for x in list_groups
+]
 
-#print(list(groups))
-print(groups2)
-#z=zip(*groups)
-#print(list(z))
+
+
+# print(list(groups))
+# print(groups2)
+z=zip(*groups2)
+# print(list(z))
+for line in z:
+    for item in line:
+        print(item.rjust(10), end='')
+    print()
